2ef06fa365ca6bfa62fd6cbc7a05c503c217d4b4
Curtisthegamedev <curtis.squire@rogers.com>
2023-01-30 15:30:55 -0500

    Started Building a test level/Hub

---
+++ Source/CapstoneProject/CJ_CharMovement.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CJ_CharMovement.generated.h"
+
+UCLASS()
+class CAPSTONEPROJECT_API ACJ_CharMovement : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACJ_CharMovement();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
